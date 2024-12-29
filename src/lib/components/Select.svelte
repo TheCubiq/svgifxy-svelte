70c6ae643618ee0f35aa5949dbdd57b31519d699
TheCubiq <jlajsek@gmail.com>
2024-12-29 03:00:43 +0100

    🧹 Update config

---
+++ src/lib/components/Select.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let options: any[] = [];
+  export let value: any;
+  export let onChange: (value: any) => void;
+</script>
+
+<select bind:value on:change={onChange}>
+  {#each options as option}
+    <option value={option}>{option}</option>
+  {/each}
+</select>
--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	vitePlugin: {
+    inspector: {
+      showToggleButton: 'always',
+      toggleButtonPos: 'bottom-right'
+    }
+  }
 };
 
 export default config;
