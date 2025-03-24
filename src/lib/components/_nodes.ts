02a7961781cafc444b460306a1eed693e5a1dab3
TheCubiq <jlajsek@gmail.com>
2025-03-24 18:55:32 +0100

    🪄 Lucide go fasttt

--- src/lib/components/_nodes.ts
+++ src/lib/components/_nodes.ts
@@ -9,7 +9,7 @@ import TurbulenceNode from './nodes/TurbulenceNode.svelte';
 import PreviewNode from './nodes/PreviewNode.svelte';
 import OffsetNode from './nodes/OffsetNode.svelte';
 import SourceGraphicNode from './nodes/SourceGraphicNode.svelte';
-import { ActivityIcon, BinaryIcon, ContrastIcon, FanIcon, icons, MoveIcon, PaintRollerIcon, ScanEyeIcon, TargetIcon, WavesIcon, WindIcon } from 'lucide-svelte';
+import { ActivityIcon, BinaryIcon, ContrastIcon, FanIcon, MoveIcon, PaintRollerIcon, ScanEyeIcon, TargetIcon, WavesIcon, WindIcon } from 'lucide-svelte';
 import DynamicNode from './nodes/DynamicNode.svelte';
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import lucidePreprocess from "vite-plugin-lucide-preprocess";
+
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		lucidePreprocess(), 
+		sveltekit(),
+	],
 	server: {
 		host: true,
 		port: 6969
